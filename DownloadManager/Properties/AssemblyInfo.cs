c586a13b3b6adf91a8e87bd109f033581dbf82d5
DuckI1ng <theducklingfn@protonmail.com>
2023-06-26 15:29:04 +0200

    Updated version info

--- DownloadManager/Properties/AssemblyInfo.cs
+++ DownloadManager/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Download Manager")]
-[assembly: AssemblyCopyright("Copyright © Duckling  2022")]
+[assembly: AssemblyCopyright("Copyright © Duckling  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: NeutralResourcesLanguage("")]
